7302a1ae6f2439ed62a0799caa2f461075cd8e01
Faiz Mokhtar <mfaizmokhtar@airasia.com>
2020-04-12 13:05:53 +0800

    update tests and add boolean operations

--- parser/parser.go
+++ parser/parser.go
@@ -72,6 +72,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
 
 	// read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -184,6 +186,10 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	expression.Right = p.parseExpression(precedence)
 
 	return expression
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
