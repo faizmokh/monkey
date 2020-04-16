90975535f561513ea662d6a06cd87b3f45024876
Faiz Mokhtar <mfaizmokhtar@airasia.com>
2020-04-16 21:34:31 +0800

    implement identifier binding

---
+++ object/environment.go
@@ -0,0 +1,22 @@
+package object
+
+// Environment is used to keep track of value
+// by associating it with a name. See `let`
+type Environment struct {
+	store map[string]Object
+}
+
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s}
+}
+
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	return obj, ok
+}
+
+func (e *Environment) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
+}
