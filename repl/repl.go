46d67486e9d2e3f25d95b8c9c8251777db8beda8
Faiz Mokhtar <mfaizmokhtar@airasia.com>
2020-04-16 21:34:31 +0800

    implement identifier binding

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/faizmokhtar/monkey/object"
+
 	"github.com/faizmokhtar/monkey/evaluator"
 	"github.com/faizmokhtar/monkey/lexer"
 	"github.com/faizmokhtar/monkey/parser"
@@ -14,6 +16,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -32,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
